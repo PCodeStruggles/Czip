4541fef04cdb87a4b2650cda076b1fda3f29091d
Paolo Arnaudo <arnaudo.paolo20@gmail.com>
2025-07-05 17:24:29 +0200

    Refactored laod content from file routine into loadFileContent func()

--- main.c
+++ main.c
@@ -4,6 +4,50 @@
 #include <string.h>
 #include <errno.h>
 
+char* loadFileContent(const char* filePath) {
+
+	FILE* fptr;
+	//Open file
+	if ((fptr = fopen(filePath, "r")) == NULL) {
+		printf("ERROR: %d %s - %s\n", errno, filePath, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	//Set cursor to file end to get file size in bytes
+	if(!(fseek(fptr, 0, SEEK_END) == 0)){
+		printf("ERROR: %d - %s\n", errno, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	//Read cursor position in bytes	
+	long int fileSize = 0;
+	if((fileSize = ftell(fptr)) == -1) {
+		printf("ERROR: %d - %s\n", errno, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	//Reset cursor position to read file from the beginning
+	fseek(fptr, 0, SEEK_SET);
+
+	//Allocate the file content buffer on the heap
+	static char* fileData;
+	if((fileData = malloc(fileSize)) == NULL) {
+		printf("ERROR: %d - %s\n", errno, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	//Read content file into the buffer pointed to by fileData
+	size_t bytesRead = 0;
+	if((bytesRead = fread(fileData, sizeof(char), fileSize, fptr)) != fileSize){
+		printf("ERROR: %d - %s\n", errno, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	//Log results
+	printf("Input file: %s\nFile size: %ld\n", filePath, fileSize);
+	printf("Number of bytes read into buffer: %zu\n", bytesRead);
+
+	//Return pointer to buffer
+	return fileData;
+}
+
 int main(int argc, char* argv[]) {
 
 	if (argc == 1) {
@@ -12,39 +56,11 @@ int main(int argc, char* argv[]) {
 	}	
 	
 	const char* filePath = argv[1];
-	FILE* fptr;
 
-	if ((fptr = fopen(filePath, "r")) == NULL) {
-		printf("ERROR: %d %s - %s\n", errno, filePath, strerror(errno));
-		return 1;
-	}
+	char* fileData = loadFileContent(filePath);
 
-	if(!(fseek(fptr, 0, SEEK_END) == 0)){
-		printf("ERROR: %d - %s\n", errno, strerror(errno));
-		return 1;
-	}
+	printf("%s\n", fileData);
 
-	long int fileSize = 0;
-	if((fileSize = ftell(fptr)) == -1) {
-		printf("ERROR: %d - %s\n", errno, strerror(errno));
-		return 1;
-	}
-
-	fseek(fptr, 0, SEEK_SET);
-
-
-	char* fileData;
-	if((fileData = malloc(fileSize)) == NULL) {
-		printf("ERROR: %d - %s\n", errno, strerror(errno));
-		return 1;
-	}
-	size_t bytesRead = 0;
-	if((bytesRead = fread(fileData, sizeof(char), fileSize, fptr)) != fileSize){
-		printf("ERROR: %d - %s\n", errno, strerror(errno));
-		return 1;
-	}
-
-	printf("Input file: %s\nFile size: %ld\n", filePath, fileSize);
-	printf("Number of bytes read into buffer: %zu\n", bytesRead);
+	free(fileData);
 	return 0;
 }
